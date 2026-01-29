8c11ace9950fac1b1ca506a199fd552ebbc7164d
nkaewam <tontan2545@gmail.com>
2026-01-29 15:30:09 +0000

    feat: dockerized

--- config.php
+++ config.php
@@ -1,15 +1,16 @@
 <?php
-// Configurazione del database
-$db_host = 'localhost';
-$db_name = 'image_catalog';
-$db_user = 'root';
-$db_pass = 'veryverystrongpassword';
+// Database configuration
+
+$db_host = getenv('DB_HOST'); 
+$db_name = getenv('DB_NAME');
+$db_user = getenv('DB_USER');
+$db_pass = getenv('DB_PASS');
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Errore di connessione: " . $e->getMessage());
+    die("Connection Error: " . $e->getMessage() . " (Code: " . $e->getCode() . ")");
 }
 
 session_start();
